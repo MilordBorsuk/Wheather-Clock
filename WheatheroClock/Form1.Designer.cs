0000d9d26f77c893422f477ddbb58bc00e1e2764
Piotr Borawski <centrumpb@gmail.com>
2019-06-12 22:17:01 +0200

    First commit

---
+++ WheatheroClock/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WheatheroClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxMinTemp = new System.Windows.Forms.TextBox();
+            this.textBoxMaxTemp = new System.Windows.Forms.TextBox();
+            this.textBoxWindspeed = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxHumidity = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wpisz miasto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Max temp:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Wiatr ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(342, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Min temp";
+            // 
+            // textBoxMinTemp
+            // 
+            this.textBoxMinTemp.Location = new System.Drawing.Point(410, 175);
+            this.textBoxMinTemp.Name = "textBoxMinTemp";
+            this.textBoxMinTemp.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMinTemp.TabIndex = 7;
+            // 
+            // textBoxMaxTemp
+            // 
+            this.textBoxMaxTemp.Location = new System.Drawing.Point(88, 172);
+            this.textBoxMaxTemp.Name = "textBoxMaxTemp";
+            this.textBoxMaxTemp.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxTemp.TabIndex = 9;
+            // 
+            // textBoxWindspeed
+            // 
+            this.textBoxWindspeed.Location = new System.Drawing.Point(88, 210);
+            this.textBoxWindspeed.Name = "textBoxWindspeed";
+            this.textBoxWindspeed.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWindspeed.TabIndex = 10;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(170, 131);
+            this.textBoxCity.Multiline = true;
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(216, 35);
+            this.textBoxCity.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(342, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Wilgotność";
+            // 
+            // textBoxHumidity
+            // 
+            this.textBoxHumidity.Location = new System.Drawing.Point(410, 210);
+            this.textBoxHumidity.Name = "textBoxHumidity";
+            this.textBoxHumidity.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHumidity.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 55);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Potwierdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(219, 240);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(104, 53);
+            this.buttonEnd.TabIndex = 17;
+            this.buttonEnd.Text = "Zakończ";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 129);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(197, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 39);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "00:00:00";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(258, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 305);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxHumidity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxWindspeed);
+            this.Controls.Add(this.textBoxMaxTemp);
+            this.Controls.Add(this.textBoxMinTemp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxMinTemp;
+        private System.Windows.Forms.TextBox textBoxMaxTemp;
+        private System.Windows.Forms.TextBox textBoxWindspeed;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxHumidity;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WheatheroClock/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace WheatheroClock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string City;
+            City = textBoxCity.Text;
+            string uri = string.Format("http://api.apixu.com/v1/current.xml?key=8e79d8c92b3f49b8a9d64504192705&q=", City);
+            XDocument doc = XDocument.Load(uri);
+
+            string iconUrl = (string)doc.Descendants("icon").FirstOrDefault();
+            WebClient client = new WebClient();
+
+            byte[] image = client.DownloadData("http:" + iconUrl );
+            MemoryStream stream = new MemoryStream(image);
+
+            Bitmap newBitmap = new Bitmap(stream);
+            string makstemp = (string)doc.Descendants("maxtemp_c").FirstOrDefault();
+            string mintemp = (string)doc.Descendants("mintemp_c").FirstOrDefault();
+           
+            string wind = (string)doc.Descendants("maxwind_mph").FirstOrDefault();
+            
+            string humidity = (string)doc.Descendants("avghumidity").FirstOrDefault();
+            Bitmap Icon = newBitmap;
+            textBoxHumidity.Text = humidity;
+            textBoxMaxTemp.Text = makstemp;
+            textBoxMinTemp.Text = mintemp;
+            textBoxWindspeed.Text = wind;
+           // kraj = "Poland";
+            pictureBox1.Image = Icon;
+            
+
+
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //label5.Text = DateTime.Now.ToLongTimeString();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            label8.Text = DateTime.Now.ToLongDateString();
+            t.Start();
+        }
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int gg = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+            string time = "";
+
+            if (gg < 10)
+            {
+                time += "0" + gg;
+            }
+            else
+            {
+                time += gg;
+            }
+            time += ":";
+            if(mm<10)
+            {
+                time += "0" + mm;
+            } else
+            {
+                time += mm;
+
+            }
+            time += ":";
+            if(ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+            time += ":";
+            label5.Text = time;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
